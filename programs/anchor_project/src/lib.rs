6479767eeae58a0ffec901c6b2b2893f124a3ead
xiaolong <longjiuwei999@163.com>
2024-03-25 17:08:00 +0800

    werited transfer sol logic

--- programs/anchor_project/src/lib.rs
+++ programs/anchor_project/src/lib.rs
@@ -1,46 +1,95 @@
+// use anchor_lang::prelude::*;
+// use std::mem::size_of;
+// declare_id!("86vnDcApQeFMx8i4vKt2pRQHWb7ZtbLL1ppRxP1hcWdu");
+
+// #[program]
+// pub mod anchor_project {
+//     use super::*;
+
+//     pub fn initialize(ctx: Context<Initialize>, key: u64) -> Result<()> {
+//         Ok(())
+//     }
+//     pub fn set(ctx: Context<Set>, key: u64, val: u64) -> Result<()> {
+//         ctx.accounts.val.value = val;
+//         Ok(())
+//     }
+// }
+
+// // 通过程序派生出来的账户
+// #[derive(Accounts)]
+// #[instruction(key: u64)]
+// pub struct Initialize<'info> {
+//     #[account(init,
+//               payer = signer,
+//               space = size_of::<Val>() + 8,
+//               // 用来推断地址
+//               seeds=[&key.to_le_bytes().as_ref()],
+//               bump)]
+//     val: Account<'info, Val>,
+
+//     #[account(mut)]
+//     signer: Signer<'info>,
+
+//     system_program: Program<'info, System>,
+// }
+
+// #[derive(Accounts)]
+// #[instruction(key: u64)]
+// pub struct Set<'info> {
+//     #[account(mut)]
+//     val: Account<'info, Val>,
+// }
+
+// #[account]
+// pub struct Val {
+//     value: u64,
+// }
+
 use anchor_lang::prelude::*;
-use std::mem::size_of;
+use anchor_lang::system_program;
+// 程序ID
 declare_id!("86vnDcApQeFMx8i4vKt2pRQHWb7ZtbLL1ppRxP1hcWdu");
 
 #[program]
 pub mod anchor_project {
     use super::*;
+    pub fn send_sol(ctx: Context<SendSol>, amount: u64) -> Result<()> {
+        msg!(
+            "system program address {:?}",
+            ctx.accounts.system_program.key()
+        );
 
-    pub fn initialize(ctx: Context<Initialize>, key: u64) -> Result<()> {
-        Ok(())
-    }
-    pub fn set(ctx: Context<Set>, key: u64, val: u64) -> Result<()> {
-        ctx.accounts.val.value = val;
-        Ok(())
+        // 调用系统程序去执行转帐指令
+        let cpi_context = CpiContext::new(
+            ctx.accounts.system_program.to_account_info(),
+            system_program::Transfer {
+                from: ctx.accounts.signer.to_account_info(),
+                to: ctx.accounts.recipient.to_account_info(),
+            },
+        );
+        let res = system_program::transfer(cpi_context, amount);
+        if res.is_ok() {
+            return Ok(());
+        } else {
+            return err!(Errors::TransferFailed);
+        }
     }
 }
 
-// 通过程序派生出来的账户
+#[error_code]
+pub enum Errors {
+    #[msg("transfer failed")]
+    TransferFailed,
+}
+
 #[derive(Accounts)]
-#[instruction(key: u64)]
-pub struct Initialize<'info> {
-    #[account(init,
-              payer = signer,
-              space = size_of::<Val>() + 8,
-              // 用来推断地址
-              seeds=[&key.to_le_bytes().as_ref()],
-              bump)]
-    val: Account<'info, Val>,
+pub struct SendSol<'info> {
+    /// CHECK: we do not read or write the data of this account
+    #[account(mut)]
+    recipient: UncheckedAccount<'info>,
+
+    system_program: Program<'info, System>,
 
     #[account(mut)]
     signer: Signer<'info>,
-
-    system_program: Program<'info, System>,
-}
-
-#[derive(Accounts)]
-#[instruction(key: u64)]
-pub struct Set<'info> {
-    #[account(mut)]
-    val: Account<'info, Val>,
-}
-
-#[account]
-pub struct Val {
-    value: u64,
 }
--- tests/anchor_project.ts
+++ tests/anchor_project.ts
@@ -7,29 +7,56 @@ describe("example_map", () => {
 
   const program = anchor.workspace.anchorProject as Program<AnchorProject>;
 
-  it("Initialize and set value", async () => {
-    const key = new anchor.BN(42);
-    const value = new anchor.BN(1337);
 
-    const seeds = [key.toArrayLike(Buffer, "le", 8)];
+  async function printAccountBalance(account) {
+    const balance = await anchor.getProvider().connection.getBalance(account);
+    console.log(`${account} has ${balance / anchor.web3.LAMPORTS_PER_SOL} SOL`);
+  }
 
-    let valueAccount = anchor.web3.PublicKey.findProgramAddressSync(
-      seeds,
-      program.programId,
-    )[0];
+  it("Transmit SOL", async () => {
 
-    console.log("value Account address", valueAccount.toBase58());
+    await printAccountBalance(anchor.getProvider().publicKey)
+    
+    // generate a new wallet
+    const recipient = anchor.web3.Keypair.generate();
+
+    await printAccountBalance(recipient.publicKey);
+
+    // send the account 1 SOL via the program
+    let amount = new anchor.BN(1 * anchor.web3.LAMPORTS_PER_SOL);
+    await program.methods.sendSol(amount)
+      .accounts({ recipient: recipient.publicKey })
+      .rpc();
 
 
-    await program.methods.initialize(key).accounts({ val: valueAccount }).rpc();
-
-    // set the account
-    await program.methods.set(key, value).accounts({ val: valueAccount }).rpc();
-
-    // read the account back
-    let result = await program.account.val.fetch(valueAccount);
-
-    console.log(`the value ${result.value} was stored in ${valueAccount.toBase58()}`);
+    await printAccountBalance(recipient.publicKey);
+    await printAccountBalance(anchor.getProvider().publicKey)
 
   });
+
+  // it("Initialize and set value", async () => {
+  //   const key = new anchor.BN(42);
+  //   const value = new anchor.BN(1337);
+
+  //   const seeds = [key.toArrayLike(Buffer, "le", 8)];
+
+  //   let valueAccount = anchor.web3.PublicKey.findProgramAddressSync(
+  //     seeds,
+  //     program.programId,
+  //   )[0];
+
+  //   console.log("value Account address", valueAccount.toBase58());
+
+
+  //   await program.methods.initialize(key).accounts({ val: valueAccount }).rpc();
+
+  //   // set the account
+  //   await program.methods.set(key, value).accounts({ val: valueAccount }).rpc();
+
+  //   // read the account back
+  //   let result = await program.account.val.fetch(valueAccount);
+
+  //   console.log(`the value ${result.value} was stored in ${valueAccount.toBase58()}`);
+
+  // });
 });
