e2769575f1a7dfd0f16802b0742b8e505b5b2a95
xiaolong <longjiuwei999@163.com>
2024-03-25 14:07:56 +0800

    writed test anchor test script

--- programs/anchor_project/src/lib.rs
+++ programs/anchor_project/src/lib.rs
@@ -1,15 +1,47 @@
 use anchor_lang::prelude::*;
+use std::mem::size_of;
+declare_id!("86vnDcApQeFMx8i4vKt2pRQHWb7ZtbLL1ppRxP1hcWdu");
 
-declare_id!("9YVEZcFtNkqyNWUNqEchHdVVLBx3rtX7W1E2cuMrwdGd");
 
 #[program]
 pub mod anchor_project {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn initialize(ctx: Context<Initialize>, key: u64) -> Result<()> {
+        Ok(())
+    }
+    pub fn set(ctx: Context<Set>, key: u64, val: u64) -> Result<()> {
+        ctx.accounts.val.value = val;
         Ok(())
     }
 }
 
+
 #[derive(Accounts)]
-pub struct Initialize {}
+#[instruction(key: u64)]
+pub struct Initialize<'info> {
+
+    #[account(init,
+              payer = signer,
+              space = size_of::<Val>() + 8,
+              seeds=[&key.to_le_bytes().as_ref()],
+              bump)]
+    val: Account<'info, Val>,
+    
+    #[account(mut)]
+    signer: Signer<'info>,
+    
+    system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+#[instruction(key: u64)]
+pub struct Set<'info> {
+    #[account(mut)]
+    val: Account<'info, Val>,
+}
+
+#[account]
+pub struct Val {
+    value: u64,
+}
--- tests/anchor_project.ts
+++ tests/anchor_project.ts
@@ -2,15 +2,34 @@ import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { AnchorProject } from "../target/types/anchor_project";
 
-describe("anchor_project", () => {
-  // Configure the client to use the local cluster.
+describe("example_map", () => {
   anchor.setProvider(anchor.AnchorProvider.env());
 
-  const program = anchor.workspace.AnchorProject as Program<AnchorProject>;
+  const program = anchor.workspace.anchorProject as Program<AnchorProject>;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+  it("Initialize and set value", async () => {
+    const key = new anchor.BN(42);
+    const value = new anchor.BN(1337);
+
+    const seeds = [key.toArrayLike(Buffer, "le", 8)];
+
+    let valueAccount = anchor.web3.PublicKey.findProgramAddressSync(
+      seeds,
+      program.programId,
+    )[0];
+
+    console.log("value Account address", valueAccount.toBase58());
+
+
+    await program.methods.initialize(key).accounts({ val: valueAccount }).rpc();
+
+    // set the account
+    await program.methods.set(key, value).accounts({ val: valueAccount }).rpc();
+
+    // read the account back
+    let result = await program.account.val.fetch(valueAccount);
+
+    console.log(`the value ${result.value} was stored in ${valueAccount.toBase58()}`);
+
   });
 });
